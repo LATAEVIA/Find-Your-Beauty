71b3364617ba26264454381e79fce5a209536045
LaTaevia Berry <lataevia.berry@gmail.com>
2016-07-25 04:50:23 -0700

    create text view on beautysactivity and set text to display zip code sent from 1st activity

--- app/src/main/java/com/lataeviaberry/mybeauty/BeautysActivity.java
+++ app/src/main/java/com/lataeviaberry/mybeauty/BeautysActivity.java
@@ -3,14 +3,18 @@ package com.lataeviaberry.mybeauty;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class BeautysActivity extends AppCompatActivity {
+    private TextView mZipCodeTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_beautys);
+        mZipCodeTextView = (TextView) findViewById(R.id.zipCodeTextView);
         Intent intent = getIntent();
         String zipCode = intent.getStringExtra("zipCode");
+        mZipCodeTextView.setText("Beauty near: " + zipCode);
     }
 }
