ddb14cb0f4a7d9a000002c06d98fd84655f02060
LaTaevia Berry <lataevia.berry@gmail.com>
2016-07-25 03:55:40 -0700

    add second activity and an intent fro main to second activity.

---
+++ app/src/main/java/com/lataeviaberry/mybeauty/BeautysActivity.java
@@ -0,0 +1,13 @@
+package com.lataeviaberry.mybeauty;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class BeautysActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_beautys);
+    }
+}
--- app/src/main/java/com/lataeviaberry/mybeauty/MainActivity.java
+++ app/src/main/java/com/lataeviaberry/mybeauty/MainActivity.java
@@ -1,13 +1,25 @@
 package com.lataeviaberry.mybeauty;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    private Button mFindBeautyButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mFindBeautyButton = (Button) findViewById(R.id.findBeautyButton);
+        mFindBeautyButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick (View v) {
+                Intent intent = new Intent(MainActivity.this, BeautysActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
