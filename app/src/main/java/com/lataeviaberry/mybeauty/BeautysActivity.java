bf81b4e11ac67f6c8cb5d2728bee8cd7a3ddc328
LaTaevia Berry <lataevia.berry@gmail.com>
2016-07-25 05:42:45 -0700

    create and initialize the ListView and set it using the .findViewById() method

--- app/src/main/java/com/lataeviaberry/mybeauty/BeautysActivity.java
+++ app/src/main/java/com/lataeviaberry/mybeauty/BeautysActivity.java
@@ -3,15 +3,20 @@ package com.lataeviaberry.mybeauty;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ListView;
 import android.widget.TextView;
 
 public class BeautysActivity extends AppCompatActivity {
     private TextView mZipCodeTextView;
+    private ListView mListView;
+    private String[] beautys = new String[] {"Nail Shop", "Hair Salon", "Organic Spa", "Massage Therapist", "Waxing Salon"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_beautys);
+
+        mListView = (ListView) findViewById(R.id.listView);
         mZipCodeTextView = (TextView) findViewById(R.id.zipCodeTextView);
         Intent intent = getIntent();
         String zipCode = intent.getStringExtra("zipCode");
