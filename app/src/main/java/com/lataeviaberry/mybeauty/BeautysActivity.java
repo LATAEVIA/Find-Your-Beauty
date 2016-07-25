e66439270d1d541cebebfe042c4125c5b0fa0b8f
LaTaevia Berry <lataevia.berry@gmail.com>
2016-07-25 05:46:00 -0700

    implement arrayadpter by
    create an ArrayAdapter and set our ListView adapter to the new adapter:

--- app/src/main/java/com/lataeviaberry/mybeauty/BeautysActivity.java
+++ app/src/main/java/com/lataeviaberry/mybeauty/BeautysActivity.java
@@ -3,6 +3,7 @@ package com.lataeviaberry.mybeauty;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
 
@@ -18,6 +19,10 @@ public class BeautysActivity extends AppCompatActivity {
 
         mListView = (ListView) findViewById(R.id.listView);
         mZipCodeTextView = (TextView) findViewById(R.id.zipCodeTextView);
+
+        ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, beautys);
+        mListView.setAdapter(adapter);
+
         Intent intent = getIntent();
         String zipCode = intent.getStringExtra("zipCode");
         mZipCodeTextView.setText("Beauty near: " + zipCode);
