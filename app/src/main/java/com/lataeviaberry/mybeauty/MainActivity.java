3ef21f36ef5baa91dede4992a5d05f123f308583
LaTaevia Berry <lataevia.berry@gmail.com>
2016-07-25 04:18:03 -0700

    remove logcat code and will pass data beautysactivity along with our Intent in the form of ‘extras’.

--- app/src/main/java/com/lataeviaberry/mybeauty/MainActivity.java
+++ app/src/main/java/com/lataeviaberry/mybeauty/MainActivity.java
@@ -3,13 +3,11 @@ package com.lataeviaberry.mybeauty;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
-    public static final String TAG = MainActivity.class.getSimpleName();
     private Button mFindBeautyButton;
     private EditText mZipCodeEditText;
 
@@ -23,8 +21,8 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick (View v) {
                 String zipCode = mZipCodeEditText.getText().toString();
-                Log.d(TAG, zipCode);
                 Intent intent = new Intent(MainActivity.this, BeautysActivity.class);
+                intent.putExtra("zipCode", zipCode);
                 startActivity(intent);
             }
         });
