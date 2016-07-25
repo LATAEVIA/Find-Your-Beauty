dc6c7aa057e019138cdeffdb917d9ad0d9d7246b
LaTaevia Berry <lataevia.berry@gmail.com>
2016-07-25 05:09:49 -0700

    add a font

--- app/src/main/java/com/lataeviaberry/mybeauty/MainActivity.java
+++ app/src/main/java/com/lataeviaberry/mybeauty/MainActivity.java
@@ -1,15 +1,18 @@
 package com.lataeviaberry.mybeauty;
 
 import android.content.Intent;
+import android.graphics.Typeface;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
     private Button mFindBeautyButton;
     private EditText mZipCodeEditText;
+    private TextView mAppNameTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,6 +20,9 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         mZipCodeEditText = (EditText) findViewById(R.id.zipCodeEditText);
         mFindBeautyButton = (Button) findViewById(R.id.findBeautyButton);
+        mAppNameTextView = (TextView) findViewById(R.id.appNameTextView);
+        Typeface madisonSquare = Typeface.createFromAsset(getAssets(), "fonts/madison-square/madison-square.ttf");
+        mAppNameTextView.setTypeface(madisonSquare);
         mFindBeautyButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick (View v) {
