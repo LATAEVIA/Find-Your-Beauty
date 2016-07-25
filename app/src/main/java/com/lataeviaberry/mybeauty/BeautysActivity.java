5936f979b50f552ecedbfc843950e12cf0406750
LaTaevia Berry <lataevia.berry@gmail.com>
2016-07-25 05:53:44 -0700

    add an OnItemClickListener that will display a toast containing the restaurant name when a list item is clicked:

--- app/src/main/java/com/lataeviaberry/mybeauty/BeautysActivity.java
+++ app/src/main/java/com/lataeviaberry/mybeauty/BeautysActivity.java
@@ -3,9 +3,12 @@ package com.lataeviaberry.mybeauty;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class BeautysActivity extends AppCompatActivity {
     private TextView mZipCodeTextView;
@@ -22,6 +25,14 @@ public class BeautysActivity extends AppCompatActivity {
 
         ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, beautys);
         mListView.setAdapter(adapter);
+
+        mListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                String beauty = ((TextView)view).getText().toString();
+                Toast.makeText(BeautysActivity.this, beauty, Toast.LENGTH_LONG).show();
+            }
+        });
 
         Intent intent = getIntent();
         String zipCode = intent.getStringExtra("zipCode");
