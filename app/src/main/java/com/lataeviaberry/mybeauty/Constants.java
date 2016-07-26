a3f0eb137a7423bf870fc636a1f80e2883adcbed
LaTaevia Berry <lataevia.berry@gmail.com>
2016-07-26 06:39:14 -0700

    add ...
    add ok http dependencies,
    permission for internet into manifest,
    java class for yelp constants
    yelp credentials to gradle
    tell module gradle to assemble yelp credentials to key value pair

---
+++ app/src/main/java/com/lataeviaberry/mybeauty/Constants.java
@@ -0,0 +1,13 @@
+package com.lataeviaberry.mybeauty;
+
+/**
+ * Created by lataeviaberry on 7/26/16.
+ */
+public class Constants {
+    public static final String YELP_CONSUMER_KEY = BuildConfig.YELP_CONSUMER_KEY;
+    public static final String YELP_CONSUMER_SECRET = BuildConfig.YELP_CONSUMER_SECRET;
+    public static final String YELP_TOKEN = BuildConfig.YELP_TOKEN;
+    public static final String YELP_TOKEN_SECRET = BuildConfig.YELP_TOKEN_SECRET;
+    public static final String YELP_BASE_URL = "https://api.yelp.com/v2/search?term=beautysvc";
+    public static final String YELP_LOCATION_QUERY_PARAMETER = "zipCode";
+}
