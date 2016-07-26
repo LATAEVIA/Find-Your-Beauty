3f861e98854b882cb3e4e2b1f990434b8befc733
LaTaevia Berry <lataevia.berry@gmail.com>
2016-07-26 07:33:58 -0700

    create model, change request url, and pace JSON then create objects

--- app/src/main/java/com/lataeviaberry/mybeauty/Constants.java
+++ app/src/main/java/com/lataeviaberry/mybeauty/Constants.java
@@ -8,7 +8,6 @@ public class Constants {
     public static final String YELP_CONSUMER_SECRET = BuildConfig.YELP_CONSUMER_SECRET;
     public static final String YELP_TOKEN = BuildConfig.YELP_TOKEN;
     public static final String YELP_TOKEN_SECRET = BuildConfig.YELP_TOKEN_SECRET;
-    public static final String YELP_BASE_URL = "https://api.yelp.com/v2/search?term=beauty";
-//    could also query category_filter=beautysvc
+    public static final String YELP_BASE_URL = "https://api.yelp.com/v2/search?category_filter=beautysvc";
     public static final String YELP_LOCATION_QUERY_PARAMETER = "location";
 }
--- app/src/main/java/com/lataeviaberry/mybeauty/YelpService.java
+++ app/src/main/java/com/lataeviaberry/mybeauty/YelpService.java
@@ -1,10 +1,18 @@
 package com.lataeviaberry.mybeauty;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
 import okhttp3.Call;
 import okhttp3.Callback;
 import okhttp3.HttpUrl;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
+import okhttp3.Response;
 import se.akerfeldt.okhttp.signpost.OkHttpOAuthConsumer;
 import se.akerfeldt.okhttp.signpost.SigningInterceptor;
 
@@ -26,5 +34,49 @@ public class YelpService {
 
         Call call = client.newCall(request);
         call.enqueue(callback);
+    }
+    public ArrayList<Beauty> processResults(Response response) {
+        ArrayList<Beauty> beautys = new ArrayList<>();
+
+        try {
+            String jsonData = response.body().string();
+            if (response.isSuccessful()) {
+                JSONObject yelpJSON = new JSONObject(jsonData);
+                JSONArray businessesJSON = yelpJSON.getJSONArray("businesses");
+                for (int i = 0; i < businessesJSON.length(); i++) {
+                    JSONObject beautyJSON = businessesJSON.getJSONObject(i);
+                    String name = beautyJSON.getString("name");
+                    String phone = beautyJSON.optString("display_phone", "Phone not available");
+                    String website = beautyJSON.getString("url");
+                    double rating = beautyJSON.getDouble("rating");
+                    String imageUrl = beautyJSON.getString("image_url");
+                    double latitude = beautyJSON.getJSONObject("location")
+                            .getJSONObject("coordinate").getDouble("latitude");
+                    double longitude = beautyJSON.getJSONObject("location")
+                            .getJSONObject("coordinate").getDouble("longitude");
+                    ArrayList<String> address = new ArrayList<>();
+                    JSONArray addressJSON = beautyJSON.getJSONObject("location")
+                            .getJSONArray("display_address");
+                    for (int y = 0; y < addressJSON.length(); y++) {
+                        address.add(addressJSON.get(y).toString());
+                    }
+
+                    ArrayList<String> categories = new ArrayList<>();
+                    JSONArray categoriesJSON = beautyJSON.getJSONArray("categories");
+
+                    for (int y = 0; y < categoriesJSON.length(); y++) {
+                        categories.add(categoriesJSON.getJSONArray(y).get(0).toString());
+                    }
+                    Beauty beauty = new Beauty(name, phone, website, rating,
+                            imageUrl, address, latitude, longitude, categories);
+                    beautys.add(beauty);
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        return beautys;
     }
 }
