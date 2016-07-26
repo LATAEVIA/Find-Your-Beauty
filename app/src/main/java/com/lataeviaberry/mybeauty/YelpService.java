6a497d3a39ecc170b38d8bf9d62dd6199d4893e2
LaTaevia Berry <lataevia.berry@gmail.com>
2016-07-26 06:56:45 -0700

    create a new class called YelpService.java inside our main package.
    create a new class called YelpService.java inside our main package.
    This class will contain all logic necessary for interacting with the
    Yelp API, including using OkHttp and SignPost to create and
    authenticate requests.
    
    First, we'll define a findBeautys() method that takes two
    parameters: The location a user provides, and a callback that will
    execute when our API request receives a readable response from Yelp.

---
+++ app/src/main/java/com/lataeviaberry/mybeauty/YelpService.java
@@ -0,0 +1,12 @@
+package com.lataeviaberry.mybeauty;
+
+import okhttp3.Callback;
+
+/**
+ * Created by lataeviaberry on 7/26/16.
+ */
+public class YelpService {
+    public static void findBeautys(String location, Callback callback) {
+
+    }
+}
