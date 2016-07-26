fc791cfed0bf53d8b55041c0cad78c1f62235fb2
LaTaevia Berry <lataevia.berry@gmail.com>
2016-07-26 07:01:26 -0700

    build out yelp service

--- app/src/main/java/com/lataeviaberry/mybeauty/YelpService.java
+++ app/src/main/java/com/lataeviaberry/mybeauty/YelpService.java
@@ -1,12 +1,30 @@
 package com.lataeviaberry.mybeauty;
 
+import okhttp3.Call;
 import okhttp3.Callback;
+import okhttp3.HttpUrl;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import se.akerfeldt.okhttp.signpost.OkHttpOAuthConsumer;
+import se.akerfeldt.okhttp.signpost.SigningInterceptor;
 
-/**
- * Created by lataeviaberry on 7/26/16.
- */
 public class YelpService {
-    public static void findBeautys(String location, Callback callback) {
+    public static void findBeautys(String zipCode, Callback callback) {
+        OkHttpOAuthConsumer consumer = new OkHttpOAuthConsumer(Constants.YELP_CONSUMER_KEY, Constants.YELP_CONSUMER_SECRET);
+        consumer.setTokenWithSecret(Constants.YELP_TOKEN, Constants.YELP_TOKEN_SECRET);
+        OkHttpClient client = new OkHttpClient.Builder()
+                .addInterceptor(new SigningInterceptor(consumer))
+                .build();
 
+        HttpUrl.Builder urlBuilder = HttpUrl.parse(Constants.YELP_BASE_URL).newBuilder();
+        urlBuilder.addQueryParameter(Constants.YELP_LOCATION_QUERY_PARAMETER, zipCode);
+        String url = urlBuilder.build().toString();
+
+        Request request= new Request.Builder()
+                .url(url)
+                .build();
+
+        Call call = client.newCall(request);
+        call.enqueue(callback);
     }
 }
