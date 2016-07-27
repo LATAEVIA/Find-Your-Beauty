1b007d0c3bd153f26f0f8e2d89bc284e12a713dd
LaTaevia Berry <lataevia.berry@gmail.com>
2016-07-27 02:20:01 -0700

    add firebase and push new zip codes a s added

--- app/src/main/java/com/lataeviaberry/mybeauty/ui/MainActivity.java
+++ app/src/main/java/com/lataeviaberry/mybeauty/ui/MainActivity.java
@@ -2,16 +2,16 @@ package com.lataeviaberry.mybeauty.ui;
 
 
 import android.content.Intent;
-import android.content.SharedPreferences;
 import android.graphics.Typeface;
 import android.os.Bundle;
-import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 import com.lataeviaberry.mybeauty.Constants;
 import com.lataeviaberry.mybeauty.R;
 
@@ -20,15 +20,22 @@ import butterknife.ButterKnife;
 
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
-    private SharedPreferences mSharedPreferences;
-    private SharedPreferences.Editor mEditor;
+//    private SharedPreferences mSharedPreferences;
+//    private SharedPreferences.Editor mEditor;
+
+    private DatabaseReference mSearchedLocationReference;
 
     @Bind(R.id.findBeautyButton) Button mFindBeautyButton;
     @Bind(R.id.zipCodeEditText) EditText mZipCodeEditText;
-    @Bind(R.id.appNameTextView) TextView mAppNameTextView;
+    @Bind(R.id.appNameTextView)
+    TextView mAppNameTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        mSearchedLocationReference = FirebaseDatabase
+                .getInstance()
+                .getReference()
+                .child(Constants.FIREBASE_CHILD_SEARCHED_LOCATION);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
@@ -36,8 +43,8 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         Typeface madisonSquare = Typeface.createFromAsset(getAssets(), "fonts/madison-square.ttf");
         mAppNameTextView.setTypeface(madisonSquare);
 
-        mSharedPreferences = PreferenceManager.getDefaultSharedPreferences(this);
-        mEditor = mSharedPreferences.edit();
+//        mSharedPreferences = PreferenceManager.getDefaultSharedPreferences(this);
+//        mEditor = mSharedPreferences.edit();
 
         mFindBeautyButton.setOnClickListener(this);
     }
@@ -46,15 +53,20 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     public void onClick(View v) {
         if (v == mFindBeautyButton) {
             String zipCode = mZipCodeEditText.getText().toString();
-            if(!(zipCode).equals("")) {
-                addToSharedPreferences(zipCode);
-            }
+            saveLocationToFirebase(zipCode);
+
+//            if(!(zipCode).equals("")) {
+//                addToSharedPreferences(zipCode);
+//            }
             Intent intent = new Intent(MainActivity.this, BeautyListActivity.class);
             intent.putExtra("zipCode", zipCode);
             startActivity(intent);
         }
     }
-    private void addToSharedPreferences(String zipCode) {
-        mEditor.putString(Constants.PREFERENCES_LOCATION_KEY, zipCode).apply();
+    public void saveLocationToFirebase(String location) {
+        mSearchedLocationReference.push().setValue(location);
     }
+//    private void addToSharedPreferences(String zipCode) {
+//        mEditor.putString(Constants.PREFERENCES_LOCATION_KEY, zipCode).apply();
+//    }
 }
